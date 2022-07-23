314c3cc3123fae980537a97c5c722f12b456b7d8
AhmedKamon <76879119+AhmedKamon@users.noreply.github.com>
2022-07-23 00:10:33 -0700

    setup

---
+++ pages/index.tsx
@@ -0,0 +1,30 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <Head>
+        <title>FAKEFLIX | HOME</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      
+      <main>
+        {/* <Banner/> */}
+        <section>
+          {/* <Row/> */}
+          {/* <Row/> */}
+          {/* <Row/> */}
+          {/* <Row/> */}
+          {/* <Row/> */}
+          {/* <Row/> */}
+          {/* <Row/> */}
+        </section>
+      </main>
+      {/* <Modal/> */}
+    </div>
+  )
+}
+
+export default Home
