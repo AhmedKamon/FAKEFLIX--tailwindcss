1d3a58650e93cecdb1d13450fc1164bbbd89d222
AhmedKamon <76879119+AhmedKamon@users.noreply.github.com>
2022-07-24 10:48:11 -0700

    fake logo

---
+++ components/Header.tsx
@@ -0,0 +1,13 @@
+
+const Header =() => {
+  return (
+    <header>
+      <div>
+        <img src='https://i.ibb.co/5KH9K3G/netflix.png' alt="" />
+      </div>
+      <div></div>
+    </header>
+  )
+}
+
+export default Header
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,18 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Header from '../components/Header'
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div>
       <Head>
         <title>FAKEFLIX | HOME</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       
       <main>
-        {/* <Banner/> */}
+        <Header/>
         <section>
           {/* <Row/> */}
           {/* <Row/> */}
