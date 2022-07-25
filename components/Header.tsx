1720f3edced3b0f63acb0f54321679cf47ccbc83
AhmedKamon <76879119+AhmedKamon@users.noreply.github.com>
2022-07-25 00:10:29 -0700

    navbar css

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,10 +2,19 @@
 const Header =() => {
   return (
     <header>
-      <div>
-        <img src='https://i.ibb.co/5KH9K3G/netflix.png' alt="" />
+      <div className="flex items-center space-x-2 md:space-x-10 " >
+        <img src='https://i.ibb.co/5KH9K3G/netflix.png' width={100} height={100} alt="" className=" cursor-pointer object-contain " />
+        <ul className=" hidden space-x-4 md:flex " >
+          <li className="headerLink" >Home</li>
+          <li className="headerLink" >TV Shows</li>
+          <li className="headerLink" >Movies</li>
+          <li className="headerLink" >New & Populer</li>
+          <li className="headerLink" >My List</li>
+        </ul>
       </div>
-      <div></div>
+      <div>
+          
+      </div>
     </header>
   )
 }
