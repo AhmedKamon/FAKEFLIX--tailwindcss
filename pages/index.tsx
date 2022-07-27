a238bb793599fcb1ccf6665a04e72d1f2fbef923
AhmedKamon <76879119+AhmedKamon@users.noreply.github.com>
2022-07-27 05:08:17 -0700

    serverSide all props

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,20 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Banner from '../components/Banner'
-import Header from '../components/Header'
-import requests from '../utils/requests'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Banner from "../components/Banner";
+import Header from "../components/Header";
+import requests from "../utils/requests";
 
 const Home: NextPage = () => {
   return (
-    <div className=' relative h-screen bg-gradient-to-b from-gray-500/10 to-[#010511] lg:h-[140vh] ' >
+    <div className=" relative h-screen bg-gradient-to-b from-gray-500/10 to-[#010511] lg:h-[140vh] ">
       <Head>
         <title>FAKEFLIX | HOME</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      
-        <Header/>
+
+      <Header />
       <main>
-        <Banner/>
+        <Banner />
         <section>
           {/* <Row/> */}
           {/* <Row/> */}
@@ -27,13 +27,22 @@ const Home: NextPage = () => {
       </main>
       {/* <Modal/> */}
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
 
-export const getServerSideProps = async () =>{
-  const [trending,netflixOriginals,topRated,actionMovies,comedyMovies,horrorMovies,romanceMovies,documentaries] = await Promise.all([
+export const getServerSideProps = async () => {
+  const [
+    trending,
+    netflixOriginals,
+    topRated,
+    actionMovies,
+    comedyMovies,
+    horrorMovies,
+    romanceMovies,
+    documentaries,
+  ] = await Promise.all([
     fetch(requests.fetchTrending).then((res) => res.json()),
     fetch(requests.fetchNetflixOriginals).then((res) => res.json()),
     fetch(requests.fetchTopRated).then((res) => res.json()),
@@ -42,5 +51,17 @@ export const getServerSideProps = async () =>{
     fetch(requests.fetchHorrorMovies).then((res) => res.json()),
     fetch(requests.fetchRomanceMovies).then((res) => res.json()),
     fetch(requests.fetchDocumentaries).then((res) => res.json()),
-  ])
-}
+  ]);
+  return {
+    props: {
+      netflixOriginals: netflixOriginals.results,
+      trending: trending.results,
+      topRated: topRated.results,
+      actionMovies: actionMovies.results,
+      comedyMovies: comedyMovies.results,
+      horrorMovies: horrorMovies.results,
+      romanceMovies: romanceMovies.results,
+      documentaries: documentaries.results,
+    },
+  };
+};
