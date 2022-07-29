af49f6047f9bbfd90d51605c6f6721fd7eb7addf
AhmedKamon <76879119+AhmedKamon@users.noreply.github.com>
2022-07-29 05:36:27 -0700

    banner

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -14,9 +14,16 @@ useEffect(() =>{
 },[netflixOriginals])
 
   return (
-    <div>
-        <div className="absolute top-0 left-0 h-[95vh] w-screen  " >
+    <div className="flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[80vh]  lg:pb-12">
+        <div className="absolute top-0 left-0 h-[95vh] w-screen -z-10  " >
             <Image src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path }`} layout='fill' objectFit="cover" />
+        </div>
+        <h1 className=" text-2xl lg:text-7xl md:text-4xl font-bold " >{ movie?.name || movie?.original_name || movie?.original_title || movie?.title }</h1>
+        <p  className=" max-w-xs text-sm md:max-w-lg lg:max-w-lg md:text-lg lg:text-2xl " > {movie?.overview} </p>
+
+        <div className="" >
+          <button className="bannerBtn" >Play</button>
+          <button className="bannerBtn" >More Info</button>
         </div>
     </div>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -25,6 +25,7 @@ const Home = ({
   romanceMovies,
  documentaries
 }: Props) => {
+  console.log(netflixOriginals)
   return (
     <div className=" relative h-screen bg-gradient-to-b from-gray-500/10 to-[#010511] lg:h-[140vh] ">
       <Head>
