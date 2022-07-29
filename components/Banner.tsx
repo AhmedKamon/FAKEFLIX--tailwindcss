549b878513ab8a58e3c8e7c061cd22c13704deaa
AhmedKamon <76879119+AhmedKamon@users.noreply.github.com>
2022-07-29 07:01:21 -0700

    text shadow

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -16,14 +16,14 @@ useEffect(() =>{
 },[netflixOriginals])
 
   return (
-    <div className="flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[80vh]  lg:pb-12">
+    <div className="flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[80vh]  lg:pb-12  ">
         <div className="absolute top-0 left-0 h-[95vh] w-screen -z-10  " >
             <Image src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path }`} layout='fill' objectFit="cover" />
         </div>
         <h1 className=" text-2xl lg:text-7xl md:text-4xl font-bold " >{ movie?.name || movie?.original_name || movie?.original_title || movie?.title }</h1>
-        <p  className=" max-w-xs text-sm md:max-w-lg lg:max-w-lg md:text-lg lg:text-2xl " > {movie?.overview} </p>
+        <p  className=" max-w-xs text-sm md:max-w-lg lg:max-w-lg md:text-lg lg:text-2xl text-shadow-md " > {movie?.overview} </p>
 
-        <div className="" >
+        <div className=" flex space-x-3 " >
           <button className="bannerBtn bg-white text-black" > <FaPlay className="h-4 w-4 md:h-7 md:w-7 text-black " /> Play</button>
           <button className="bannerBtn bg-[gray]/70 " > <InformationCircleIcon className="h-5 w-5 md:h-8 md:w-8 text-black " /> More Info</button>
         </div>
